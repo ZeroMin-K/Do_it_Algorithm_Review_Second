311905aaf32ad4edfe00ac2d638331f0b14d06c0
ZeroMin-K <77002670+ZeroMin-K@users.noreply.github.com>
2021-09-08 23:09:12 +0900

    Update judge_sign.py

--- chapter01/judge_sign.py
+++ chapter01/judge_sign.py
@@ -1,14 +1,10 @@
-def med3(a, b, c):
-    # a, b, c의 중앙값을 구하여 반환
-    if (b >= a and c <= a) or (b <= a and c >= a):
-        return a
-    elif (a > b and c < b) or ( a < b and c > b):
-        return b
-    return c
+# 입력받은 정수의 부호 출력
 
-print('세 정수의 중앙값 계산')
-a = int(input('정수 a 값 입력: '))
-b = int(input('정수 b 값 입력: '))
-c = int(input('정수 c 값 입력: '))
+n = int(input('정수 입력: '))
 
-print(f'중앙값 {med3(a, b, c)}')
+if n > 0:
+    print('양수')
+elif n < 0:
+    print('음수')
+else:
+    print('0')
