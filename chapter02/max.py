d9c65eb8de571fecb2d1f6e52161983c0b904134
ZeroMin-K <77002670+ZeroMin-K@users.noreply.github.com>
2021-10-04 22:36:50 +0900

    Create max.py

---
+++ chapter02/max.py
@@ -0,0 +1,19 @@
+# 시퀀스 원소의 최댓값 출력하기
+
+from typing import Any, Sequence
+def max_of(a: Sequence) -> Any:
+    # 시퀀스형 a 원소의 최댓값을 반환
+    maximum = a[0]
+    for i in range(1, len(a)):
+        if a[i] > maximum:
+            maximum = a[i]
+    return maximum
+
+if __name__ == '__main__':
+    print('배열의 최댓값 계산')
+    num  = int(input('원소 수 입력: '))
+    x = [None] * num
+    for i in range(num):
+        x[i] = int(input(f'x[{i}] 값 입력: '))
+
+    print(f'최댓값 : {max_of(x)}')
