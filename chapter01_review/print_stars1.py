c5264033f3f446fcea4b44a55f0c765cb45f27bb
ZeroMin-K <77002670+ZeroMin-K@users.noreply.github.com>
2022-01-10 23:27:38 +0900

    Create print_stars1.py

---
+++ chapter01_review/print_stars1.py
@@ -0,0 +1,11 @@
+print('* 출력')
+n = int(input('몇 개 출력?: '))
+w = int(input('몇 개바다 줄바꿈?: '))
+
+for i in range(n):
+    print('*', end='')
+    if i % w == w-1:
+        print()
+
+if n % w:
+    print()
